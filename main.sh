4d1142ff0f11c518b5f7f0fc71ce5ba151c760e4
北川 <82112667+LinnBenson@users.noreply.github.com>
2026-01-02 16:43:24 +0800

    Add main.sh for server management with options

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -e "\033[1m\033[34mTodu Shell 服务器管理\033[0m"
+echo -e "\033[34mVersion 1.0.0\033[0m"
+echo "==============="
+echo "1. Screen 会话管理"
+echo ""
+
+read -p "请选择需要执行的功能: " choice
+case "$choice" in
+    1) bash <(curl -Ls https://raw.githubusercontent.com/LinnBenson/LinuxShell/main/screen.sh);;
+    *) echo -e "\033[41m[Error] 当前选择不存在！\033[0m";;
+esac
